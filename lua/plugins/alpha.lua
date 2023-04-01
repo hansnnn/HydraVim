d5f51af102930082c840fa6970ecf57d19b2d5dc
Diogo-ss <ssdiogo@proton.me>
2023-04-01 17:38:38 -0300

    add: alpha

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,42 @@
+return {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    cmd = "Alpha",
+    config = function()
+        local alpha = require("alpha")
+
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val =
+        {
+            " ",
+            "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆         ",
+            "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+            "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷⠄⠄⠄⠄⠻⠿⢿⣿⣧⣄     ",
+            "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+            "          ⢠⣿⣿⣿⠈  ⠡⠌⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+            "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘⠄ ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+            "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+            " ⣠⣿⠿⠛⠄⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+            " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇⠄⠛⠻⢷⣄ ",
+            "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+            "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+            "         ⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃     ",
+            " ",
+        }
+        dashboard.section.buttons.val = {
+            dashboard.button( "n", "  - New file" , ":ene <BAR> startinsert <CR>"),
+            dashboard.button( "f", "  - Find file", ":Telescope find_files<CR>"),
+            dashboard.button( "r", "  - Recent"   , ":Telescope oldfiles<CR>"),
+            dashboard.button( "s", "  - Settings" , ":ex $MYVIMRC | :cd %:p:h <CR>"),
+            dashboard.button( "q", "  - Quit", ":qa<CR>"),
+        }
+
+        alpha.setup(dashboard.opts)
+
+        vim.cmd([[
+            autocmd FileType alpha setlocal nofoldenable
+        ]])
+
+    end
+}
